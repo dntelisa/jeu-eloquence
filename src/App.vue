1b9c8d22ef216f0911a0a156cc33b3cb312d7822
Elisa <elisabzh10@gmail.com>
2026-05-23 22:28:46 +0200

    première carte

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import EchauffementCard from './echauffement/EchauffementCard.vue';
+
+</script>
 
 <template>
   <h1>PALABRUME</h1>
+  <EchauffementCard />
 </template>
 
-<style>
-html, body {
-  margin: 0;
-  padding: 0;
-}
-
+<style scoped>
 h1 {
   display: flex;
   background-color: #42b983;
@@ -19,4 +18,11 @@ h1 {
   font-family: Arial, Helvetica, sans-serif;
   align-items: center;
 }
+</style>
+
+<style>
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
 </style>
---
+++ src/echauffement/EchauffementCard.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div class="echauffement-card">
+    <h1>Echauffement</h1>
+    <img src="../assets/images/echauffement.png" alt="Echauffement" />
+    <p>Echauffe toi en répettant des phrases le plus rapidement possible.</p>
+  </div>
+</template>
+
+<style scoped>
+.echauffement-card {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f0f0f0;
+  padding: 20px;
+  border-radius: 10px;
+  box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
+  width: 300px;
+  margin: 20px;
+
+  img {
+    width: 100px;
+    height: 100px;
+    margin-bottom: 20px;
+  }
+
+}
+
+</style>

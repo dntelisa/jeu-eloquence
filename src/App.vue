9ef5aa0c8641a683996f5a4b5c03cd04a19b37d8
Elisa <elisabzh10@gmail.com>
2026-05-23 22:08:44 +0200

    definition du nom et de l'icon

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,22 @@
 <script setup lang="ts"></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <h1>PALABRUME</h1>
 </template>
 
-<style scoped></style>
+<style>
+html, body {
+  margin: 0;
+  padding: 0;
+}
+
+h1 {
+  display: flex;
+  background-color: #42b983;
+  justify-content: center;
+  margin: 0;
+  height: 100px;
+  font-family: Arial, Helvetica, sans-serif;
+  align-items: center;
+}
+</style>
